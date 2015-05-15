10d31240151e08e1b90909eddad3bf48953482da
Kenji Kamimura <gzcamg@gmail.com>
2015-05-15 11:01:33 +0900

    Initial Commit

---
+++ GLVection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLVection
+//
+//  Created by Kenji Kamimura on 2015/05/15.
+//  Copyright (c) 2015年 Kenji Kamimura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
