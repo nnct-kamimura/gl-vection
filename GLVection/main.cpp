e242bf9b404852e4a56c769d7a5c1e5ca3b1a75c
Kenji Kamimura <kamimura@nagaoka-ct.ac.jp>
2015-05-21 15:09:23 +0900

    Under development

--- GLVection/main.cpp
+++ GLVection/main.cpp
@@ -7,16 +7,25 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 
 // glut is now deprecated
 //  thus change it to GLFW
 #include "GLFW/glfw3.h"
 #include <OpenGL/OpenGL.h>
-
 //  To use GLFW: fink, brew or port install glfw.
 //  Folowing frameworks are required.
 //      CoreVideo, IOKit, Cocoa, OpenGL
 //   and include libglfw.dylib to build phase.
+
+// function at exitting program
+static void cleanup()
+{
+    // finalize GLFW
+    glfwTerminate();
+}
+
+
 int main(int argc, const char * argv[]) {
     // initialize GLFW
     if (glfwInit() == GL_FALSE)
@@ -26,5 +35,26 @@ int main(int argc, const char * argv[]) {
         return EXIT_FAILURE;
     }
 
+    // 終了時処理
+    atexit(cleanup);
+    
+    // obtaine monitors for later use.
+    int m_count;
+    GLFWmonitor **monitors = glfwGetMonitors(&m_count);
+    
+    // とりあえず何も考えずにプライマリモニタを利用
+    const GLFWvidmode *mode = glfwGetVideoMode(monitors[0]);
+    
+    // create window
+    GLFWwindow *const window(glfwCreateWindow(mode->width, mode->height, "GL Vection", monitors[0], NULL));
+    //GLFWwindow *const window(glfwCreateWindow(640, 480, "GL Vection", NULL, NULL));
+    
+    if (window == NULL)
+    {
+        // fail in window creation
+        std::cerr << "Can't create GLFW window." << std::endl;
+        return EXIT_FAILURE;
+    }
+    
     return EXIT_SUCCESS;
 }
