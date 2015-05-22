495aa4e8e83ecb17f9395bc1ba09d45826ee0d3f
Kenji Kamimura <kamimura@nagaoka-ct.ac.jp>
2015-05-22 11:06:07 +0900

    基礎設計完了。 ・位相変更はテクスチャマッピングの座標変更で対応。 ・今後描画エリアを狭める必要があるときは，このオブジェクトの手前にマスクを描画することで対応する

--- GLVection/main.cpp
+++ GLVection/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <math.h>
 
 // glut is now deprecated
 //  thus change it to GLFW
@@ -25,6 +26,7 @@ static void cleanup()
     glfwTerminate();
 }
 
+#define PERIODLEN 512
 
 int main(int argc, const char * argv[]) {
     // initialize GLFW
@@ -35,8 +37,17 @@ int main(int argc, const char * argv[]) {
         return EXIT_FAILURE;
     }
 
-    // 終了時処理
+    // 終了時処理の登録
     atexit(cleanup);
+    
+    // Sin波の作成
+    GLubyte sinwave[PERIODLEN][3];
+    for (int indx=0; indx < PERIODLEN; indx++)
+    {
+        sinwave[indx][0] = ((sin((double)indx/PERIODLEN*2*M_PI)+1)/2)*255;
+        sinwave[indx][1] = ((sin((double)indx/PERIODLEN*2*M_PI)+1)/2)*255;
+        sinwave[indx][2] = ((sin((double)indx/PERIODLEN*2*M_PI)+1)/2)*255;
+    }
     
     // obtaine monitors for later use.
     int m_count;
@@ -58,14 +69,60 @@ int main(int argc, const char * argv[]) {
     
     // set the window as openGL target
     glfwMakeContextCurrent(window);
+
+    GLuint textureS;
+    glGenTextures(1, &textureS);
+    glBindTexture(GL_TEXTURE_1D, textureS);
     
+    // data transfere mode
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    
+    // texture assign
+    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, PERIODLEN, 0, GL_RGB, GL_UNSIGNED_BYTE, sinwave);
+    
+    // テクスチャ拡大縮小オプション
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
     // set clear color
     glClearColor(0.0, 0.0, 0.0, 0.0);
     
+    // configure
+    glEnable(GL_DEPTH_TEST);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1, 1, -1, 1, 0, 5);
+    glMatrixMode(GL_MODELVIEW);
+//    glEnable(GL_LIGHTING);
+//    glEnable(GL_LIGHT0);
     // rendering loop
+    
+    // control variables
+    double vArea = 1.0;
+    double hArea = 1.0;
+    int cycles = 5;
+    double verocity = -0.05;    // 運動右向きが負
+    double texpos = 0.0;
     while (glfwWindowShouldClose(window) == GL_FALSE && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        
+        // calc texture pos
+        texpos += verocity;
+        if (texpos >= 1.0) texpos = 0.0;
+        if (texpos <= -1.0) texpos = 0.0;
+        
+        glEnable(GL_TEXTURE_1D);
+        //glNormal3d(0, 0, 1);
+        glBegin(GL_QUADS);
+        glTexCoord2d(texpos, 0); glVertex3d(-hArea, -vArea, -1);
+        glTexCoord2d(texpos, 1); glVertex3d(-hArea, vArea, -1);
+        glTexCoord2d(texpos+cycles, 1); glVertex3d(hArea, vArea, -1);
+        glTexCoord2d(texpos+cycles, 0); glVertex3d(hArea, -vArea, -1);
+        glEnd();
+        glDisable(GL_TEXTURE_1D);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
